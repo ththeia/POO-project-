60e7b0c5312ada55c3f1e39366ccb9f454ad9ae5
Theia <100139847+ththeia@users.noreply.github.com>
2022-12-06 23:00:51 +0200

    Phase 1 final commit

--- POO Project/BuildingClasses.h
+++ POO Project/BuildingClasses.h
@@ -3,173 +3,187 @@
 #include <string>
 using namespace std;
 
-class Table {
-	string tableName;
-	int noColumns;
-	Column* column;
-public:
-
-	Table() {
-
-	}
-
-	Table(string tableName, int noColumns, int noRows, Column* column) :
-		tableName(tableName), noColumns(noColumns), noRows(noRows)
-	{
-		if (this->column != nullptr)
-		{
-			delete[]this->column;
-			this->column = new Column[this->noColumns];
-		}
-		for (int i = 0; i < this->noColumns; i++)
-		{
-			this->column[i] = column[i];
-		}
-	}
-
-	string getTableName() {
-		return this->getTableName;
-	}
-
-	void setTableName(string Name){
-		this->getTableName = Name;
-	}
-	
-};
-
-class Data {
-	int v1 = 0;
-	float v2 = 0;
-	string text = "";
-public:
-	Data(){}
-	~Data(){}
-};
-
-//Class for creating a column
+enum type { TEXT = 1, INTEGER = 2, FLOAT = 3 };
 class Column {
 public:
 	string name = "";
-	int noOfRows = 0;
+	type columnType;
+	int noRows = 0;
 	int intValue = 0;
 	float floatValue = 0;
-	string* textValue =nullptr;
+	string textValue ;
 
 public:
 
-	Column(string Name, int noOfRows) {
-		setName(Name);
-		setNoOfRows(noOfRows);
-	}
-
-	//Default constructor
 	Column() {
 		this->name = "No column name";
-		this->~columnType = type::INTEGER;
-		this->noOfRows = 0;
+		this->columnType = type::INTEGER;
+		this->noRows = 0;
 	}
-	//Constructor
-	Column(string Name, int noOfRows, int Value) :name(Name), noOfRows(noOfRows) {
-		//this->name = Name;
-		//this->dimension = Dimension;
+	Column(string name, type Type, int row) {
+		this->name = name;
+		this->columnType = Type;
+		this->noRows = row;
 	}
 
-	Column(string Name, int noOfRows, int Value) :name(Name), noOfRows(noOfRows) {
+	Column(string name, type Type, int row, int Value) {
+		this->name = name;
+		this->columnType = columnType;
+		this->noRows = noRows;
 		if (this->getType() == "integer")
-			this->textValue = Value;
+			this->intValue = Value;
 	}
 
-	Column(string Name, int noOfRows, float Value) :name(Name), noOfRows(noOfRows) {
+	Column(string Name, type Type, int row, float floatValue) {
+		this->name = Name;
+		this->columnType = columnType;
+		this->noRows = noRows;
 		if (this->getType() == "float")
-			this->textValue = Value;
+			this->floatValue = floatValue;
 	}
 
-	Column(string Name, int noOfRows, string Value) :name(Name), noOfRows(noOfRows) {
+	Column(string Name, type Type, int row, string textValue) {
+		this->name = Name;
+		this->columnType = columnType;
+		this->noRows = noRows;
 		if (this->getType() == "text")
-			this->textValue = Value;
+		this->textValue = textValue;
 	}
 
-	//Copy Constructor
-	Column(const Column& object) {
-		this->name = object.name;
-		this->noOfRows = object.noOfRows;
-		//this->value=object.value;
+
+	Column(const Column& copy) {
+		this->name = copy.name;
+		this->columnType = copy.columnType;
+		this->noRows = copy.noRows;
+
 	}
 
-	//Destructor
-	~Column()
-	{
-		if (textValue != nullptr)
-			delete[]textValue;
+
+	~Column() {
+
 	}
 
 	string getName() {
 		return this->name;
 	}
-	void setName(string name) {
-		this->name = name;
+	void setName(string Name) {
+		this->name = Name;
 	}
-	int getNoOfRows() {
-		return this->noOfRows;
+	int getDimension() {
+		return this->noRows;
 	}
-	void setNoOfRows(int noOfRows) {
-		this->noOfRows = noOfRows;
+	void setDimension(int dim) {
+		this->noRows = dim;
+	}
+	string getType() {
+		if (Column::columnType == 1) {
+			return "text";
+		}
+		else if (Column::columnType == 2) {
+			return "integer";
+		}
+		else return "float";
+	}
+	type getColumnType() {
+		return this->columnType;
 	}
 
 };
 
-class DataBase {
-	int noOfTables;
-	string* tableNames;
-public:
-	DataBase() 
-	{
-		int noOfTables = 0;
-		string* tableNames = "";
-	}
-	void createTable(string tableName)
-	{
-		this->noOfTables = this->noOfTables + 1;
-		string* newTableNames = new string[this->noOfTables];
-		for (int i = 0; i < this->noOfTables - 1; i++)
-		{
-			newTableNames[i] = this->tableNames[i];
-		}
-		for (int j = this->noOfTables - 1; j < this->noOfTables; j++)
-		{
-			newTableNames[j] = tableName[j - this->noOfTables - 1];
-		}
-		this->tableNames = new string[this->noOfTables];
-		for (int i = 0; i < this->noOfTables; i++)
-		{
-			this->tableNames[i] = newTableNames[i];
-		}
-		delete[] newTableNames;
+	class Table {
+		string tableName;
+		int noColumns;
+		int noRows;
+		Column* column;
+	public:
 
-	}
-	void dropTable(string tableName)
-	{
-		for (int i = 0; i < this->noOfTables; i++)
-		{
-			if (this->tableNames[i] == tableName)
+		Table() {
+
+		}
+
+		Table(string tableName, int noColumns, int noRows, Column* column) {
+			this->tableName = tableName;
+			this->noColumns = noColumns;
+			this->noRows = noRows;
+			this->column = new Column[this->noColumns];
+			for (int i = 0; i < this->noColumns; i++)
 			{
-				for (int j = i; j < this->noOfTables - 1; j++)
-				{
-					this->tableNames[j] = this->tableNames[j + 1];
-				}
+				this->column[i] = column[i];
 			}
-			this->noOfTables--;
 		}
-	}
 
-	string* getTableNames()
-	{
-		return this->tableNames;
-	}
-	int noOfTables() 
-	{
-		return this->noOfTables;
-	}
+		string getTableName() {
+			return this->tableName;
+		}
 
-};
+		void setTableName(string Name) {
+			this->tableName = Name;
+		}
+
+	};
+
+	class Data {
+		int v1 = 0;
+		float v2 = 0;
+		string text = "";
+	public:
+		Data() {}
+		~Data() {}
+	};
+
+	class DataBase {
+		int noOfTables;
+		string* tableNames;
+	public:
+		DataBase()
+		{
+			noOfTables = 0;
+			tableNames = nullptr;
+		}
+		void createTable(string tableName)
+		{
+			this->noOfTables = this->noOfTables + 1;
+			string* newTableNames = new string[this->noOfTables];
+			for (int i = 0; i < this->noOfTables - 1; i++)
+			{
+				newTableNames[i] = this->tableNames[i];
+			}
+			for (int j = this->noOfTables - 1; j < this->noOfTables; j++)
+			{
+				newTableNames[j] = tableName[j - this->noOfTables - 1];
+			}
+			this->tableNames = new string[this->noOfTables];
+			for (int i = 0; i < this->noOfTables; i++)
+			{
+				this->tableNames[i] = newTableNames[i];
+			}
+			delete[] newTableNames;
+
+		}
+		void dropTable(string tableName)
+		{
+			for (int i = 0; i < this->noOfTables; i++)
+			{
+				if (this->tableNames[i] == tableName)
+				{
+					for (int j = i; j < this->noOfTables - 1; j++)
+					{
+						this->tableNames[j] = this->tableNames[j + 1];
+					}
+				}
+				this->noOfTables--;
+			}
+		}
+
+		string* getTableNames()
+		{
+			return this->tableNames;
+		}
+	
+		int getNoOfTables()
+		{
+			return this->noOfTables;
+		}
+
+	};
 
--- POO Project/Source.cpp
+++ POO Project/Source.cpp
@@ -1,8 +1,18 @@
-#include<BuildingClasses.h>
+#include "BuildingClasses.h"
 #include <iostream>
+#include<string>
+
+
 using namespace std;
+
+class Validate : public exception {
+public:
+	Validate(string message) : exception(message.c_str()) {
+
+	}
+};
 
 
 int main() {
-	//...
+	
 }
